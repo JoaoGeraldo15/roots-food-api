72da09ed458cbfe5258021c7a7dfa4aa1c32a213
joaogeraldo15 <joaogeraldo40@gmail.com>
2024-01-03 20:48:28 -0300

    Core: Initial project

---
+++ src/main/java/com/rootsfood/demo/RootsfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.rootsfood.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootsfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RootsfoodApiApplication.class, args);
+	}
+
+}
